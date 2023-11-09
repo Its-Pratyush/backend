269da764283ebfc452590452aca226d73a08eb81
Pratyush gogoi <96740074+Its-Pratyush@users.noreply.github.com>
2023-11-10 04:09:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 //available routes
-
+app.get("/", (req,res)=>{
+  res.send("Hello")
+}
+)
 app.use("/api/auth", require("./Routes/auth"));
 app.use("/api/notes", require("./Routes/notes"));
 app.use("/api/todos", require("./Routes/todo"));
